f3481ec0cf5c309fb3f4486d669b73341c53e981
Patrick Palmer <ppalmer@pm.me>
2023-11-16 22:35:53 -0600

    Add DynamoDB table

--- cdk/constructs/public-api.ts
+++ cdk/constructs/public-api.ts
@@ -1,63 +1,126 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import path = require('path');
 import { Construct } from 'constructs';
 
-export class PublicApi extends Construct {
-  constructor(scope: Construct, id: string) {
+export interface PublicApiProps {
+  table: dynamodb.Table;
+}
+
+export class PublicRestApi extends Construct {
+  private api: apigateway.RestApi;
+  private table: dynamodb.Table;
+  private powertoolsLambdaLayer: lambda.ILayerVersion;
+
+  constructor(scope: Construct, id: string, props: PublicApiProps) {
     super(scope, id);
 
+    this.table = props.table;
+    this.initializeApi();
+    this.addConsultationRequest();
+  }
+
+  private initializeApi() {
+    const apiLoggingRole = new iam.Role(this, 'ApiGatewayRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromManagedPolicyArn(
+          this,
+          'ApiGatewayLoggingPolicy',
+          'arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs'
+        )
+      ]
+    });
+
+    const account = new apigateway.CfnAccount(this, 'ApiGatewayAccount', {
+      cloudWatchRoleArn: apiLoggingRole.roleArn
+    });
+
+    const apiGatewayLogGroup = new logs.LogGroup(this, 'ApiGatewayLogGroup', {
+      retention: logs.RetentionDays.ONE_WEEK
+    });
+
+    this.api = new apigateway.RestApi(this, 'PublicApi', {
+      restApiName: 'Public API',
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(
+          apiGatewayLogGroup
+        ),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields()
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key'
+        ]
+      }
+    });
+
+    this.api.node.addDependency(account);
+
+    const publicKey = this.api.addApiKey('PublicApiKey', {
+      description: 'Public API key for unauthenticated users'
+    });
+
+    const publicUsagePlan = this.api.addUsagePlan('PublicUsagePlan', {
+      name: 'Public Usage Plan',
+      apiStages: [{ stage: this.api.deploymentStage }],
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 2
+      }
+    });
+
+    publicUsagePlan.addApiKey(publicKey);
+
     // https://docs.powertools.aws.dev/lambda/typescript/latest/
-    const powertoolsLayer = lambda.LayerVersion.fromLayerVersionArn(
+    this.powertoolsLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(
       this,
       'PowertoolsLayer',
       `arn:aws:lambda:${
         cdk.Stack.of(this).region
       }:094274105915:layer:AWSLambdaPowertoolsTypeScript:23`
     );
+  }
 
-    const consultationRequestHandler = new lambda.Function(
+  private addConsultationRequest() {
+    const requestConsultation = new lambda.Function(
       this,
-      'ConsultationRequestHandler',
+      'RequestConsultation',
       {
         code: lambda.Code.fromAsset(
-          path.join('esbuild.out', 'sendConsultationRequest')
+          path.join('esbuild.out', 'requestConsultation')
         ),
-        handler: 'sendConsultationRequest.handler',
-        layers: [powertoolsLayer],
+        handler: 'requestConsultation.handler',
+        layers: [this.powertoolsLambdaLayer],
         runtime: lambda.Runtime.NODEJS_18_X,
         architecture: lambda.Architecture.ARM_64,
+        environment: {
+          APP_TABLE_NAME: this.table.tableName,
+          POWERTOOLS_SERVICE_NAME: 'requestConsultation'
+        }
       }
     );
 
-    const api = new apigateway.RestApi(this, 'PublicApi', {
-      restApiName: 'Public API',
-    });
-
-    const publicKey = api.addApiKey('PublicApiKey', {
-      description: 'Public API key for unauthenticated website visitors',
-    });
-
-    const publicUsagePlan = api.addUsagePlan('PublicUsagePlan', {
-      name: 'Public Usage Plan',
-      apiStages: [{ stage: api.deploymentStage }],
-      throttle: {
-        rateLimit: 10,
-        burstLimit: 2,
-      },
-    });
-
-    publicUsagePlan.addApiKey(publicKey);
+    this.table.grantReadData(requestConsultation);
+    this.table.grantWriteData(requestConsultation);
 
     const consultationRequestIntegration = new apigateway.LambdaIntegration(
-      consultationRequestHandler
+      requestConsultation
     );
 
-    api.root
+    this.api.root
       .addResource('consultation-request')
       .addMethod('POST', consultationRequestIntegration, {
-        apiKeyRequired: true,
+        apiKeyRequired: true
       });
   }
 }
--- cdk/stacks/app-stack.ts
+++ cdk/stacks/app-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { AmplifyNextjs } from '../constructs/amplify-nextjs';
-import { PublicApi } from '../constructs/public-api';
+import { DynamoDbTable } from '../constructs/dynamodb-table';
+import { PublicRestApi } from '../constructs/public-api';
+// import { AmplifyNextjs } from '../constructs/amplify-nextjs';
 
 export interface AppStackProps extends cdk.StackProps {
   amplifyAppName: string;
@@ -24,6 +25,14 @@ export class AppStack extends cdk.Stack {
 
     super(scope, id, stackProps);
 
+    const okWildscapesDb = new DynamoDbTable(this, 'OkWildscapesDatabase', {
+      tableName: 'ok-wildscapes'
+    });
+
+    new PublicRestApi(this, 'OkWildscapesPublicApi', {
+      table: okWildscapesDb.table
+    });
+
     // TODO shut down pre-existing Amplify app
     // new AmplifyNextjs(this, 'OkWildscapesNextJs', {
     //   amplifyAppName: amplifyAppName,
@@ -32,7 +41,5 @@ export class AppStack extends cdk.Stack {
     //   branch: branch,
     //   githubTokenName: githubTokenName,
     // });
-
-    new PublicApi(this, 'OkWildscapesPublicApi');
   }
 }
