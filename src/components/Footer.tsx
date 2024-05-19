74973255079edffe75c793535b6da3bae482247d
Patrick Palmer <ppalmer@pm.me>
2024-05-19 10:09:02 -0500

    Add resources page with link to invasive plant db

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import { NavLinks } from '@/components/NavLinks';
 import { JSX, SVGProps } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import ExternalLink from './ExternalLink';
 
 const navigation = [
   {
@@ -36,7 +37,7 @@ const navigation = [
 
 export function Footer() {
   return (
-    <footer className="border-t border-gray-200">
+    <footer className="border-t border-gray-200 bg-neutral-50">
       <Container>
         <div className="flex flex-col items-start gap-y-12 py-12">
           <div className="flex flex-col-reverse sm:flex-row justify-between items-center w-full text-gray-900">
@@ -52,37 +53,8 @@ export function Footer() {
             {/* ONPN link */}
             <div className="flex flex-col-reverse items-center text-sm">
               <p className="max-w-60 pt-2 text-center">
-                We are a proud member of the Oklahoma Native Plant Network!
-                <Link
-                  href="https://www.onpn.org/"
-                  className="text-emerald-600 hover:text-emerald-700"
-                  target="_blank"
-                >
-                  {' '}
-                  Learn more
-                  <span className="inline-flex">
-                    <svg
-                      data-testid="geist-icon"
-                      fill="none"
-                      height="24"
-                      shape-rendering="geometricPrecision"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      viewBox="0 0 24 24"
-                      width="24"
-                      style={{
-                        color: 'currentColor',
-                        width: '14px',
-                        height: '14px'
-                      }}
-                    >
-                      <path d="M7 17L17 7"></path>
-                      <path d="M7 7h10v10"></path>
-                    </svg>
-                  </span>
-                </Link>
+                We are a proud member of the Oklahoma Native Plant Network!{' '}
+                <ExternalLink text="Learn more" href="https://www.onpn.org/" />
               </p>
               <Link href="https://www.onpn.org/" target="_blank">
                 <Image
