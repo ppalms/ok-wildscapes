ba4ae217be2ea63a9cb819191a8131cb7ab81b30
Patrick Palmer <ppalmer@pm.me>
2024-05-23 01:44:39 -0500

    Add What to Expect and How to Maintain to resources page

--- src/app/(main)/resources/page.tsx
+++ src/app/(main)/resources/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/ui/main/Container';
 import { ExternalLink } from '@/ui/main/ExternalLink';
+import Link from 'next/link';
 
 export default function Page() {
   return (
@@ -13,14 +14,34 @@ export default function Page() {
             Information about native plants and wildscaping
           </p>
           <section>
-            <div className="mt-4">
-              <p>
+            <div className="mt-6">
+              <h2 className="font-display text-3xl tracking-tight text-gray-900">
+                Links
+              </h2>
+              <p className="border-t pt-2">
                 Learn about invasive plants at the Oklahoma Invasive Plant
                 Council's{' '}
                 <ExternalLink
                   text="invasive plant database"
                   href="https://www.okinvasives.org/plants-database"
                 />
+              </p>
+            </div>
+          </section>
+          <section>
+            <div className="mt-6">
+              <h2 className="font-display text-3xl tracking-tight text-gray-900">
+                Documents
+              </h2>
+              <p className="border-t pt-2">
+                What to Expect and How to Maintain{' - '}
+                <Link
+                  href={`${process.env.NEXT_PUBLIC_ASSETS_URL}/what-to-expect-and-how-to-maintain.pdf`}
+                  className="text-emerald-600 hover:text-emerald-700"
+                  target="_blank"
+                >
+                  Download PDF
+                </Link>
               </p>
             </div>
           </section>
