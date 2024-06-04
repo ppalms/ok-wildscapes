24b25d40c7ac99ff58cc3ee885c0e8d5a3204bbc
Patrick Palmer <ppalmer@pm.me>
2024-06-04 11:51:02 -0500

    Add upload button spinner and adjust form padding

--- src/app/(backoffice)/plant-sheets/upload/page.tsx
+++ src/app/(backoffice)/plant-sheets/upload/page.tsx
@@ -1,11 +1,9 @@
-'use client';
-
 import UploadForm from './upload-form';
 
 const Page = () => {
   return (
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto pb-4">
         <div className="flex items-baseline justify-between">
           <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight mt-8 mb-4 lg:text-5xl">
             New Plant Sheet
